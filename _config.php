4fb10178bb432d099d1c2012a99d6145faef31a3
Zvudkdn <168515482+Zvudkdn@users.noreply.github.com>
2024-07-23 16:34:03 +0530

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,29 @@
+<?php 
+
+$websiteTitle = "AniKaizoku"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
+$contactEmail = "admin.anikaizoku.com"; // Contact Email
+
+$version = "4.3";
+
+//Donate 
+$donate = "#";
+
+// Socials 
+$telegram = "https://t.me/anikaizoku"; // telegram
+$discord = "https://discord.com/#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://indianime.disqus.com"; // Disqus
+
+
+// API URL
+$api = "https://anikatsu-api-mu.vercel.app"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
